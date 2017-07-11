93f147fdd18a39514fa97fd3a4aef1e53ff0d5aa
Ruben Duran <rd@rubenduran.net>
2017-07-11 13:37:40 -0700

    Initial commit

--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -6,20 +6,4 @@
 //  Copyright © 2017 R&R. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
+import Foundation
